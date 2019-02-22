1100d6b9c9df640d0a3261e7f09d1eed1fb5c62f
LinChiaYuan <lo919201@gmail.com>
2019-02-22 09:36:55 +0800

    Initial commit

---
+++ src/distancecount.java
@@ -0,0 +1,22 @@
+
+public class distancecount
+{
+    public static void main(String[] args)
+    {
+        double lat_b = 24.115841;
+        double lng_b = 120.667959;
+        double lat_a = 24.1813744; //24.11599446;
+        double lng_a = 120.647535; //120.66792073;
+
+        double radLat1 = (lat_a * Math.PI / 180.0);
+        double radLat2 = (lat_b * Math.PI / 180.0);
+        double a = radLat1 - radLat2;
+        double b = (lng_a - lng_b) * Math.PI / 180.0;
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2)
+                + Math.cos(radLat1) * Math.cos(radLat2)
+                * Math.pow(Math.sin(b / 2), 2)));
+        s = s * 6378137.0;
+        s = Math.round(s * 10000) / 10000;
+        System.out.println(s);
+    }
+}
